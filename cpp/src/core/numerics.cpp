98ae48c2c2aebad60f328b66ce5a3142864f8719
Haram Kim <44259000+Haram-kim@users.noreply.github.com>
2019-08-26 22:41:50 +0900

    Add nermerics for hat unhat operation

---
+++ cpp/src/core/numerics.cpp
@@ -0,0 +1,18 @@
+
+inline Eigen::Matrix3d skew(const Eigen::Vector3d& v)
+{
+    Eigen::Matrix3d M;
+    M << 0, -v(2), v(1),
+           v(2), 0, -v(0),
+           -v(1), v(0), 0;
+
+    return M;
+}
+
+inline Eigen::Vector3d vec(const Eigen::Matrix3d& M)
+{
+    Eigen::Vector3d v;
+    v << M(2,1), M(0,2), M(1,0);
+    return v;
+}
+
